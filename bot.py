27970b48c6a86aea8c0a433aecece0aaff3a569d
webellionTechnology <webelliontechnology@gmail.com>
2025-06-07 14:33:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,7 +114,7 @@ async def party_register(ctx, party_name: str, party_hex: str):
         await ctx.send("Invalid Party Name")
 
 @bot.command(name="business-register")
-async def business_register(ctx, business_name: str, business_hex: str)
+async def business_register(ctx, business_name: str, business_hex: str):
     if len(party_hex) == 7:
         user_id = ctx.author.id
         nickname = ctx.author.nick or ctx.author.name
